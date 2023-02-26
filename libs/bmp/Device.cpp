57b2b2ab2df183c0eefa6fc6d15e9774da0c4e6d
Mac Millan <nmacmil1@jhu.edu>
2023-02-26 16:38:36 -0500

    culling BMP SPI stuff.

--- libs/bmp/Device.cpp
+++ libs/bmp/Device.cpp
@@ -1,4 +1,5 @@
 /*
+	I2C Version - mac
   Device is an I2C/SPI compatible base class library.
 	
 	Copyright (C) Martin Lindupp 2020
@@ -32,24 +33,13 @@
 	SOFTWARE.
 */
 
-#include <Device.h>
+#include "Device.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // Device Class Constructors
 ////////////////////////////////////////////////////////////////////////////////
 
-Device::Device(TwoWire& twoWire) : comms(I2C_COMMS), i2c(&twoWire) {}								// Initialise constructor for I2C communications
-#ifdef ARDUINO_ARCH_ESP8266
-Device::Device(uint8_t sda, uint8_t scl, TwoWire& twoWire) : 												// Constructor for ESP8266 I2C with user-defined pins
-	comms(I2C_COMMS_DEFINED_PINS), i2c(&twoWire), sda(sda), scl(scl) {}	
-#endif
-Device::Device(uint8_t cs) : comms(SPI_COMMS), cs(cs), spiClockSpeed(1000000) {}		// Constructor for SPI communications
-#ifdef ARDUINO_ARCH_ESP32																														
-Device::Device(uint8_t sda, uint8_t scl, TwoWire& twoWire) : 												// Constructor for ESP32 I2C with user-defined pins
-	comms(I2C_COMMS_DEFINED_PINS), i2c(&twoWire), sda(sda), scl(scl) {}	
-Device::Device(uint8_t cs, uint8_t spiPort, SPIClass& spiClass) 										// Constructor for ESP32 HSPI communications
-	: comms(SPI_COMMS), cs(cs), spiPort(spiPort), spi(&spiClass), spiClockSpeed(1000000) {}
-#endif
+Device::Device(TwoWire& twoWire) : i2c(&twoWire) {}								// Initialise constructor for I2C communications
 
 ////////////////////////////////////////////////////////////////////////////////
 // Device Public Member Function
@@ -57,66 +47,18 @@ Device::Device(uint8_t cs, uint8_t spiPort, SPIClass& spiClass) 										// Con
 
 void Device::setClock(uint32_t clockSpeed)													// Set the I2C or SPI clock speed
 {
-	if (comms == I2C_COMMS)
-	{
-		i2c->setClock(clockSpeed);
-	}
-	else
-	{
-		spiClockSpeed = clockSpeed;
-	}
+	i2c->setClock(clockSpeed);
 }
 
-#if !defined ARDUINO_ARCH_ESP8266 && !defined ARDUINO_ARCH_ESP32 && !defined ARDUINO_SAM_DUE && !defined STM32F1
-void Device::usingInterrupt(uint8_t pinNumber)											// Wrapper for the SPI usingInterrupt() function
-{
-	spi->usingInterrupt(pinNumber);
-}
-
-void Device::notUsingInterrupt(uint8_t pinNumber)										// Wrapper for the SPI notUsingInterrupt() function
-{
-	spi->notUsingInterrupt(pinNumber);
-}
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////
-// Device I2C & SPI Wrapper (Protected) Member Functions
+// Device I2C (Protected) Member Functions
 ////////////////////////////////////////////////////////////////////////////////
 
 void Device::initialise()																						// Initialise device communications
 {
-  if (comms == I2C_COMMS)																						// Check with communications bus has been selected I2C or SPI
-	{
-		i2c->begin();																										// Initialise I2C communication
-		i2c->setClock(400000);																					// Set the SCL clock to default of 400kHz
-	}
-#if defined ARDUINO_ARCH_ESP8266 || defined ARDUINO_ARCH_ESP32
-	else if (comms == I2C_COMMS_DEFINED_PINS)													// Check if the ESP8266 has specified user-defined I2C pins
-	{
-		i2c->begin(sda, scl);																						// Initialise I2C communication with user-defined pins
-		i2c->setClock(400000);																					// Set the SCL clock to default of 400kHz
-		comms = I2C_COMMS;																							// Set the communications to standard I2C
-	}
-#endif
-	else
-	{
-		digitalWrite(cs, HIGH);																					// Pull the chip select (CS) pin high
-		pinMode(cs, OUTPUT);																						// Set-up the SPI chip select pin
-#ifdef ARDUINO_ARCH_ESP32
-		if (spiPort == HSPI)																						// Set-up spi pointer for VSPI or HSPI communications
-		{
-			spi->begin(14, 27, 13, 2);																		// Start HSPI on SCK 14, MOSI 13, MISO 24, SS CS (GPIO2 acts as dummy pin)
-		}
-		else
-		{
-			spi = &SPI;																										// Start VSPI on SCK 5, MOSI 18, MISO 19, SS CS
-			spi->begin();
-		}														
-#else
-		spi = &SPI;																											// Set-up spi pointer for SPI communications
-		spi->begin();
-#endif
-	}
+	i2c->begin();																										// Initialise I2C communication
+	i2c->setClock(400000);																					// Set the SCL clock to default of 400kHz
 }
 
 void Device::setI2CAddress(uint8_t addr)														// Set the Device's I2C address
@@ -126,70 +68,33 @@ void Device::setI2CAddress(uint8_t addr)														// Set the Device's I2C ad
 
 void Device::writeByte(uint8_t subAddress, uint8_t data)
 {
-  if (comms == I2C_COMMS)
-	{
-		i2c->beginTransmission(address);  															// Write a byte to the sub-address using I2C
-		i2c->write(subAddress);          
-		i2c->write(data);                 
-		i2c->endTransmission();          
-	}
-	else // if (comms == SPI_COMMS)
-	{
-		spi->beginTransaction(SPISettings(spiClockSpeed, MSBFIRST, SPI_MODE0));	// Write a byte to the sub-address using SPI
-		digitalWrite(cs, LOW);
-		spi->transfer(subAddress & WRITE_MASK);
-		spi->transfer(data);
-		digitalWrite(cs, HIGH);
-		spi->endTransaction();
-	}
+	i2c->beginTransmission(address);  															// Write a byte to the sub-address using I2C
+	i2c->write(subAddress);          
+	i2c->write(data);                 
+	i2c->endTransmission();          
 }
 
 uint8_t Device::readByte(uint8_t subAddress)												// Read a byte from the sub-address using I2C
 {
   uint8_t data = 0x00;
-	if (comms == I2C_COMMS)																		
-	{
-		i2c->beginTransmission(address);         
-		i2c->write(subAddress);                  
-		i2c->endTransmission(false);             
-		i2c->requestFrom(address, (uint8_t)1);	 
-		data = i2c->read();                      
-	}
-	else // if (comms == SPI_COMMS)
-	{
-		spi->beginTransaction(SPISettings(spiClockSpeed, MSBFIRST, SPI_MODE0));		// Read a byte from the sub-address using SPI
-		digitalWrite(cs, LOW);
-		spi->transfer(subAddress | READ_MASK);
-		spi->transfer(0x00);																						// Read dummy byte required by BMP388 for SPI
-		data = spi->transfer(data);
-		digitalWrite(cs, HIGH);
-		spi->endTransaction();	
-	}
+	i2c->beginTransmission(address);         
+	i2c->write(subAddress);                  
+	i2c->endTransmission(false);             
+	i2c->requestFrom(address, (uint8_t)1);	 
+	data = i2c->read();                      
   return data;                             													// Return data read from sub-address register
 }
 
 void Device::readBytes(uint8_t subAddress, uint8_t* data, uint16_t count)
 {  
-  if (comms == I2C_COMMS)																						// Read "count" bytes into the "data" buffer using I2C
+  // Read "count" bytes into the "data" buffer using I2C
+	i2c->beginTransmission(address);          
+	i2c->write(subAddress);                   
+	i2c->endTransmission(false);              
+	uint8_t i = 0;
+	i2c->requestFrom(address, (uint8_t)count);  
+	while (i2c->available()) 
 	{
-		i2c->beginTransmission(address);          
-		i2c->write(subAddress);                   
-		i2c->endTransmission(false);              
-		uint8_t i = 0;
-		i2c->requestFrom(address, (uint8_t)count);  
-		while (i2c->available()) 
-		{
-			data[i++] = i2c->read();          
-		}
-	}
-	else // if (comms == SPI_COMMS)		
-	{
-		spi->beginTransaction(SPISettings(spiClockSpeed, MSBFIRST, SPI_MODE0));	// Read "count" bytes into the "data" buffer using SPI
-		digitalWrite(cs, LOW);
-		spi->transfer(subAddress | READ_MASK);
-		spi->transfer(0x00);																						// Read dummy byte required by BMP388 for SPI
-		spi->transfer(data, count);
-		digitalWrite(cs, HIGH);
-		spi->endTransaction();	
+		data[i++] = i2c->read();          
 	}
 }
