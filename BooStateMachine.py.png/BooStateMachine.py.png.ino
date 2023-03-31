57d8f6901aebe9d2b17c09d0d814a172d6b01011
Janya Budaraju <budaraju.janya@gmail.com>
2023-03-30 20:22:04 -0400

    added broadcast

--- BooStateMachine.py.png/BooStateMachine.py.png.ino
+++ BooStateMachine.py.png/BooStateMachine.py.png.ino
@@ -1,11 +1,51 @@
 // Define the possible states for each set
 enum Recovery_State { IDLE_REC, DETECT_LAUNCH_IGN, VALID_FLIGHT_REC, DEPLOY_MAIN_REC };
 
+
+//struct for packet
+typedef struct {
+  uint16_t xGyro; //4
+  uint16_t yGyro; //4
+  uint16_t zGyro; //4
+  uint16_t xAcc; //4
+  uint16_t yAcc; //4
+  uint16_t zAcc; //4
+
+} __attribute__((packed)) IMU;
+
+typedef struct {
+  char header = 0x55; //1
+  char padding0; //1
+  char stage; //1
+  char padding1; //1
+  uint32_t latitude; //4
+  char padding2; //1
+  uint32_t longitude; //4
+  char padding3; //1
+  uint32_t altitude; //4
+  char padding4; //1
+  IMU imuPacket; // 24
+} __attribute__((packed)) Packet;
+
+const int intended_packet_size = sizeof(Packet);
+
 // Define the initial state for each set
 StateSet1 Recovery_State = IDLE_REC;
 
 void setup() {
-  // Initialize your hardware and other setup code here
+    //Lora Setup
+  Serial.begin(115200);
+  while (!Serial);
+  if (!LoRa.begin(915E6)) {
+    while (1);
+  }
+}
+
+void send_to_lora(uint8_t * packet) {
+  //writing with packet
+  LoRa.beginPacket();
+  LoRa.write(packet, intended_packet_size);
+  LoRa.endPacket();
 }
 
 
@@ -16,6 +56,7 @@ float HEIGHT_FLOOR = 0.00776714 //TEMP height floor in 8-miles height floor
 float MAIN_TRIGGER = 0.0142045 //TEMP height floor in 8-miles main trigger
 
 void loop() {
+  
   // PLACEHOLDER READ DATA
   data = read_telemetry();
   // Update state machine Recovery State
@@ -48,5 +89,29 @@ void loop() {
       break;
     broadcast_data();
   }
-  send_packet();
+}
+
+void broadcast_data(){
+  //set up IMU packet
+  IMU imuP;
+  imuP.xGyro = 0;
+  imuP.yGyro = 0;
+  imuP.zGyro = 0;
+  imuP.xAcc = 0;
+  imuP.yAcc = 0;
+  imuP.zAcc = 0;
+
+  //set up transmission Packet
+  Packet packet;
+  packet.stage = '1';
+  packet.latitude = 0;
+  packet.longitude = 10;
+  packet.altitude = 0;
+  packet.imuPacket = imuP;
+
+  //convert to uint8_t packet
+  uint8_t * packet_addr = (uint8_t *)(&packet);
+
+  //send to lora function
+  send_to_lora(packet_addr);
 }
