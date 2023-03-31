7ff34eda888f32f5b3403ea9aca3a9c6a08a9ffe
Owen <oreed2@jhu.edu>
2023-03-30 20:15:50 -0400

    Implemented Dummy Send Packet

--- SusStateMachine.py/SusStateMachine.py.ino
+++ SusStateMachine.py/SusStateMachine.py.ino
@@ -6,10 +6,62 @@ enum Ignition_State { IDLE_IGN, DETECT_LAUNCH_IGN, VALID_FLIGHT_IGN, LOCKOUT_IGN
 StateSet1 Recovery_State = IDLE_REC;
 StateSet2 Ignition_State = IDLE_IGN;
 
+
+#include <LoRa.h>
+
+/*
+stage label bit (1 bit)
+status (1 byte, chars)
+GPS data —> 2 32 bit long  (lat & longitude)
+altimeter —> int 32 bit
+IMU (gyro & accelerometer) —> 6 16 bit integers 
+*/
+
+//struct for packet
+typedef struct {
+  uint16_t xGyro; //4
+  uint16_t yGyro; //4
+  uint16_t zGyro; //4
+  uint16_t xAcc; //4
+  uint16_t yAcc; //4
+  uint16_t zAcc; //4
+
+} __attribute__((packed)) IMU;
+
+typedef struct {
+  char header = 0x55; //1
+  char padding0; //1
+  char stage; //1
+  char padding1; //1
+  uint32_t latitude; //4
+  char padding2; //1
+  uint32_t longitude; //4
+  char padding3; //1
+  uint32_t altitude; //4
+  char padding4; //1
+  IMU imuPacket; // 24
+} __attribute__((packed)) Packet;
+
+//struct for transmission
+const int intended_packet_size = sizeof(Packet);
+
+
+
 void setup() {
-  // Initialize your hardware and other setup code here
+  //Lora Setup
+  Serial.begin(115200);
+  while (!Serial);
+  if (!LoRa.begin(915E6)) {
+    while (1);
+  }
 }
 
+void send_to_lora(uint8_t * packet) {
+  //writing with packet
+  LoRa.beginPacket();
+  LoRa.write(packet, intended_packet_size);
+  LoRa.endPacket();
+}
 
 //ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent isses with sideways travel
 
@@ -95,6 +147,31 @@ void loop() {
       //END STATE
       break;
   }
-  send_packet();
   // Add any other code you want to run continuously in your loop here
+}
+
+
+void broadcast_data() {
+  //set up IMU packet
+  IMU imuP;
+  imuP.xGyro = 0;
+  imuP.yGyro = 0;
+  imuP.zGyro = 0;
+  imuP.xAcc = 0;
+  imuP.yAcc = 0;
+  imuP.zAcc = 0;
+
+  //set up transmission Packet
+  Packet packet;
+  packet.stage = '1';
+  packet.latitude = 0;
+  packet.longitude = 10;
+  packet.altitude = 0;
+  packet.imuPacket = imuP;
+
+  //convert to uint8_t packet
+  uint8_t * packet_addr = (uint8_t *)(&packet);
+
+  //send to lora function
+  send_to_lora(packet_addr);
 }
