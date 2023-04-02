40ff772bd583b42604445fb22c932f3e70671f25
Janya Budaraju <budaraju.janya@gmail.com>
2023-04-02 15:14:40 -0400

    bug fixes

--- BooStateMachine.py.png/BooStateMachine.py.png.ino
+++ BooStateMachine.py.png/BooStateMachine.py.png.ino
@@ -1,6 +1,7 @@
+#include <LoRa.h>
+
 // Define the possible states for each set
 enum Recovery_State { IDLE_REC, DETECT_LAUNCH_IGN, VALID_FLIGHT_REC, DEPLOY_MAIN_REC };
-
 
 //struct for packet
 typedef struct {
@@ -30,7 +31,7 @@ typedef struct {
 const int intended_packet_size = sizeof(Packet);
 
 // Define the initial state for each set
-StateSet1 Recovery_State = IDLE_REC;
+Recovery_State StateSet1 = IDLE_REC;
 
 void setup() {
     //Lora Setup
@@ -52,39 +53,44 @@ void send_to_lora(uint8_t * packet) {
 //ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent isses with sideways travel
 
 //ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
-float HEIGHT_FLOOR = 0.00776714 //TEMP height floor in 8-miles height floor
-float MAIN_TRIGGER = 0.0142045 //TEMP height floor in 8-miles main trigger
+float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles height floor
+float MAIN_TRIGGER = 0.0142045; //TEMP height floor in 8-miles main trigger
 
 void loop() {
   
   // PLACEHOLDER READ DATA
-  data = read_telemetry();
+  int data[] = {0,0,0,0,0,0,0};
+  //data = read_telemetry();
   // Update state machine Recovery State
-  switch (Recovery_State) {
+  switch (StateSet1) {
     case IDLE_REC: //State to represent the rocket pre launch 
       //Placeholder condition for detecting launch
-      if (data[acceleration] >= 0){ //movement of rocket is the current assumption for launch
-        Recovery_State = DETECT_LAUNCH_IGN;
+      //0 = acceleration index
+      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
+        StateSet1 = DETECT_LAUNCH_IGN;
       }
       break;
     case DETECT_LAUNCH_IGN: //state to represent the rocket post launch (can mistake heavy jostling for launch)
       //Placeholder condition for confirming launch
-      if data[altitude] >= HEIGHT_FLOOR{ //floor reached
-        Recovery_State = VALID_FLIGHT_REC;
+      //1 = altitude 
+      if (data[1] >= HEIGHT_FLOOR) { //floor reached
+        StateSet1 = VALID_FLIGHT_REC;
       }
       //detect false launch 
-      if (data[altitude_trend] == 0){//if not tredning anywhere
-        Recovery_State = IDLE_REC;
+      //2 = altitude_trend
+      if (data[2] == 0) {//if not tredning anywhere
+        StateSet1 = IDLE_REC;
       }
       break;
     case VALID_FLIGHT_REC: //State to represent further confirmation of launch with heigth floor
       //Placeholder condition for detecting apogee and triggering drouge deploy
-      if (data[altitude_trend] < 0){//if treding down
-        Recovery_State = DEPLOY_DROGUE_REC;
+      // 2 = atitude trend 
+      if (data[2] < 0) {//if treding down
+        StateSet1 = DEPLOY_MAIN_REC;
       }
       break;
     case DEPLOY_MAIN_REC: //state to trigger main deploy at floor
-      do_main();
+      //deploy_main();
       //END STATE
       break;
     broadcast_data();
--- SusStateMachine.py/SusStateMachine.py.ino
+++ SusStateMachine.py/SusStateMachine.py.ino
@@ -1,11 +1,12 @@
+#include <LoRa.h>
+
 // Define the possible states for each set
-enum Recovery_State { IDLE_REC, DETECT_LAUNCH_IGN, VALID_FLIGHT_REC, DEPLOY_DROGUE_REC, DEPLOY_MAIN_REC };
+enum Recovery_State { IDLE_REC, DETECT_LAUNCH_REC, VALID_FLIGHT_REC, DEPLOY_DROGUE_REC, DEPLOY_MAIN_REC };
 enum Ignition_State { IDLE_IGN, DETECT_LAUNCH_IGN, VALID_FLIGHT_IGN, LOCKOUT_IGN, IGNITE_IGN };
 
 // Define the initial state for each set
-StateSet1 Recovery_State = IDLE_REC;
-StateSet2 Ignition_State = IDLE_IGN;
-
+Recovery_State StateSet1 = IDLE_REC;
+Ignition_State StateSet2 = IDLE_IGN;
 
 #include <LoRa.h>
 
@@ -45,8 +46,6 @@ typedef struct {
 //struct for transmission
 const int intended_packet_size = sizeof(Packet);
 
-
-
 void setup() {
   //Lora Setup
   Serial.begin(115200);
@@ -66,84 +65,88 @@ void send_to_lora(uint8_t * packet) {
 //ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent isses with sideways travel
 
 //ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
-float HEIGHT_FLOOR = 0.00776714 //TEMP height floor in 8-miles height floor
-float MAIN_TRIGGER = 0.0142045 //TEMP height floor in 8-miles main trigger
+float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles height floor
+float MAIN_TRIGGER = 0.0142045; //TEMP height floor in 8-miles main trigger
 
 void loop() {
   // PLACEHOLDER READ DATA
-  data = read_telemetry();
+  //data = read_telemetry();
+  int data[] = {0,0,0,0,0,0,0};
   // Update state machine Recovery State
-  switch (Recovery_State) {
+  switch (StateSet1) {
     case IDLE_REC: //State to represent the rocket pre launch 
       //Placeholder condition for detecting launch
-      if (data[acceleration] >= 0){ //movement of rocket is the current assumption for launch
-        Recovery_State = DETECT_LAUNCH_IGN;
+      //0 = acceleration index
+      if (data[0] >= 0) { //movement of rocket is the current assumption for launch
+        StateSet1 = DETECT_LAUNCH_REC;
       }
       break;
     case DETECT_LAUNCH_IGN: //state to represent the rocket post launch (can mistake heavy jostling for launch)
       //Placeholder condition for confirming launch
-      if data[altitude] >= HEIGHT_FLOOR{ //floor reached
-        Recovery_State = VALID_FLIGHT_REC;
+      //1 = altitude 
+      if (data[1] >= HEIGHT_FLOOR) { //floor reached
+        StateSet1 = VALID_FLIGHT_REC;
       }
       //detect false launch 
-      if (data[altitude_trend] == 0){//if not tredning anywhere
-        Recovery_State = IDLE_REC;
+      //2 = altitude_trend
+      if (data[2] == 0) {//if not tredning anywhere
+        StateSet1 = IDLE_REC;
       }
       break;
     case VALID_FLIGHT_REC: //State to represent further confirmation of launch with heigth floor
       //Placeholder condition for detecting apogee and triggering drouge deploy
-      if (data[altitude_trend] < 0){//if treding down
-        Recovery_State = DEPLOY_DROGUE_REC;
+      if (data[2] < 0) {//if treding down
+        StateSet1 = DEPLOY_DROGUE_REC;
       }
       break;
     case DEPLOY_DROGUE_REC: //State to trigger drouge deploy at apogee
-      do_drouge();
-      if (data[altitude] <= MAIN_TRIGGER){
-        Recovery_State = DEPLOY_MAIN_REC; 
+      //deploy_drogue();
+      if (data[1] <= MAIN_TRIGGER) {
+        StateSet1 = DEPLOY_MAIN_REC; 
       }
       break;
     case DEPLOY_MAIN_REC: //state to trigger main deploy at floor
-      do_main();
+      //deploy_main();
       //END STATE
       break;
     broadcast_data();
   }
 
   // Update state machine for Ignition State
-  switch (Ignition_State) {
+  switch (StateSet2) {
     case IDLE_IGN:
       //Placeholder condition for detecting launch
-      if (data[acceleration] >= 0){ //movement of rocket is the current assumption for launch
-        Recovery_State = DETECT_LAUNCH_IGN;
+      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
+        StateSet2 = DETECT_LAUNCH_IGN;
       }
       break;
     case DETECT_LAUNCH_IGN:
       //Placeholder condition for confirming launch
-      if data[altitude] >= HEIGHT_FLOOR{ //floor reached
-        Recovery_State = VALID_FLIGHT_REC;
+      if (data[1] >= HEIGHT_FLOOR) { //floor reached
+        StateSet2 = VALID_FLIGHT_IGN;
       }
-      //detect false launch 
-      if (data[altitude_trend] == 0){//if not tredning anywhere
-        Recovery_State = IDLE_REC;
+      //detect false launch  (we dropped the rocket)
+      if (data[2] == 0) {//if not tredning anywhere
+        StateSet2 = IDLE_IGN;
       }
       break;
     case VALID_FLIGHT_IGN:
       //if any tilt exists
-      if (abs(data[tilt]) >= 5) {//
-        Recovery_State = LOCKOUT_IGN;
+      if (abs(data[3]) >= 5) {//
+        StateSet2 = LOCKOUT_IGN;
       }
       // detect time to ignite
-      if (data[acceleration_change] < 0){//if slowing down
-        Recovery_State = IGNITE_IGN;
+      if (data[4] < 0) {//if slowing down
+        StateSet2 = IGNITE_IGN;
       }
       break;
-      Ignition_State = LOCKOUT_IGN;
+      StateSet2 = LOCKOUT_IGN;
       break;
     case LOCKOUT_IGN:
       //END STATE
       break;
     case IGNITE_IGN:
-      do_ignite();
+      //do_ignite();
       //END STATE
       break;
   }
