4f1eb3b70195d1208700d5f23d8c3afa4369bb27
Janya Budaraju <budaraju.janya@gmail.com>
2023-04-16 15:14:50 -0400

    serial comms

--- dataGen.py
+++ dataGen.py
@@ -1,44 +1,72 @@
 import csv
+from csv import reader
 import random
+import serial
+import time
 
-alt_index = 0
-zAcc = 1000
 
 with open('launch1.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    field = ["zAcc", "altTrend", "altitude"]
+    field = ["theta", "phi", "xAcc", "yAcc", "zAcc", "latitude", "longitude", "altitude", "altTrend"]
     writer.writerow(field)
+
+    theta = phi = xAcc = yAcc = zAcc = latitude = longitude = alt_index = alt_trend = 0
+    variability = 0
 
     #pre launch
     for i in range(10):
-        writer.writerow(["0.0", "0", "0.0"])
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
 
+    zAcc = 1000
+    alt_trend = 1
     #until acceleration becomes negative
     for i in range(100):
-        alt_index += 15
-        zAcc -= 11
-        variability = random.uniform(1, 3)
-        writer.writerow([zAcc - variability, 1, alt_index + variability])
-
+        #variability = random.uniform(1, 3)
+        alt_index += 15 + variability
+        zAcc -= 11 - variability
+        
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
+    
     #sustainer fire, acceleration grows super fast
     for i in range(15):
-        alt_index += 20
-        zAcc += 100
-        variability = random.uniform(1, 3)
-        writer.writerow([zAcc - variability, 1, alt_index + variability])
+        #variability = random.uniform(1, 3)
+        alt_index += 20 + variability
+        zAcc += 100 - variability
+        
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
 
     #until apogee
     for i in range(50):
-        alt_index += 15
-        zAcc -= 11
-        variability = random.uniform(1, 3)
-        writer.writerow([zAcc - variability, 1, alt_index + variability])
+        #ariability = random.uniform(1, 3)
+        alt_index += 15 + variability
+        zAcc -= 11 - variability
+        
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
 
+    alt_trend = -1
     while(alt_index > 0):
-        alt_index -= 15
-        variability = random.uniform(1, 3)
-        zAcc -= 4
-        writer.writerow([zAcc - variability, -1, alt_index + variability])
+        #variability = random.uniform(1, 3)
+        alt_index -= 15 + variability
+        zAcc -= 4 - variability
 
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
+
+    theta = phi = xAcc = yAcc = zAcc = latitude = longitude = alt_index = alt_trend = 0
     for i in range(10):
-        writer.writerow(["0.0", "0", "0.0"])
+        writer.writerow([theta, phi, xAcc, yAcc, zAcc, latitude, longitude, alt_index, alt_trend])
+
+ser = serial.Serial('/dev/ttyUSB0')
+
+# skip the first line(the header)
+with open('launch1.csv', 'r') as file:
+    file_csv = reader(file)
+    head = next(file_csv)
+    
+    # check if the file is empty or not
+    if head is not None:
+        # Iterate over each row
+        for row in file_csv:
+            time.sleep(.2)
+            ser.write(row) 
+    
+ser.close()
