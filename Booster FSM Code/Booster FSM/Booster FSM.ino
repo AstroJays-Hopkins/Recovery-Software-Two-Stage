519b51cecd1e55e76ae51b7eb0775d65b8586da8
Owen <oreed2@jhu.edu>
2023-10-01 19:43:59 -0400

    10-1 Workshop: Fixes to Arduino get data

--- Booster FSM Code/Booster FSM/Booster FSM.ino
+++ Booster FSM Code/Booster FSM/Booster FSM.ino
@@ -2,6 +2,11 @@
 
 // Define the possible states for each set
 enum Recovery_State { IDLE_REC = 'I', DETECT_LAUNCH_IGN = 'D', VALID_FLIGHT_REC = 'F', DEPLOY_MAIN_REC = 'M'};
+// Define the LED pins
+const int ledPin1 = 2;
+const int ledPin2 = 3;
+const int ledPin3 = 4;
+
 
 //struct for packet
 typedef struct {
@@ -36,85 +41,8 @@ const int intended_packet_size = sizeof(Packet);
 // Define the initial state for each set
 Recovery_State StateSet1 = IDLE_REC;
 
-//Setup for state machine
-void setup() {
-    //Attempt Lora setup
-  Serial.begin(115200);
-  while (!Serial);
-  if (!LoRa.begin(915E6)) {
-    while (1);
-  }
-}
 
-//method to write to LoRa
-void send_to_lora(uint8_t * packet) {
-  //writing with packet
-  LoRa.beginPacket();
-  LoRa.write(packet, intended_packet_size);
-  LoRa.endPacket();
-}
-
-//TODO::ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent issues with sideways travel
-
-//ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
-float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles
-float MAIN_TRIGGER = 0.0142045; //TEMP main trigger in 8-miles
-
-void loop() {
-  // PLACEHOLDER READ DATA [TODO::Actually get the data]
-  //variable assignment: {acceleration index, altitude, altitude_trend}
-  int data[] = {0,0,0,0,0,0,0};
-  data = pull_test_data();
-
-  //TODO::update method --> data = read_telemetry();
-
-  // Update state machine 
-  switch (StateSet1) {
-    case IDLE_REC: //State to represent the rocket pre launch 
-      //Placeholder condition for detecting launch
-      //index[0] = acceleration index
-      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
-        StateSet1 = DETECT_LAUNCH_IGN;
-      }
-      break;
-
-    //State to represent the rocket post launch (can mistake heavy jostling for launch)
-    case DETECT_LAUNCH_IGN: 
-      //Placeholder condition for confirming launch
-      //index[1] = altitude 
-      //Check if height floor reached and set to valid launch
-      if (data[1] >= HEIGHT_FLOOR) {
-        StateSet1 = VALID_FLIGHT_REC;
-      }
-      //detect false launch 
-      //index[2] = altitude_trend
-      if (data[2] == 0) {
-        //if altitude is not changing
-        StateSet1 = IDLE_REC;
-      }
-      break;
-    
-    //State to represent further confirmation of launch with height floor
-    case VALID_FLIGHT_REC: 
-      //index[2] = atitude trend 
-      //check if altitude is trending downwards
-      if (data[2] < 0) {
-        StateSet1 = DEPLOY_MAIN_REC;
-      }
-      break;
-
-    //State to trigger main deploy at floor
-    case DEPLOY_MAIN_REC: 
-      //deploy_main();
-      //END STATE
-      break;
-    
-  }
-  send_test_data(StateSet1);
-  //broadcast_data(); //REMOVED FOR PYTHON TESTING
-}
-
-int[] pull_test_data(){
+void pull_test_data(int *formatInputString){
   char inputString[25]; // declare a variable to store the input string
   int i = 0;
   while (Serial.available()) { // check if there is any data available in the serial port
@@ -123,22 +51,22 @@ int[] pull_test_data(){
       break; // if yes, exit the loop
     }
     inputString[i] = c; // append the character to the input string
-    i++
+    i++;
   }
   inputString[i] = '\0';
   int formatData[9];
   char c;
   int buff = 0;
-  int i = 0;
+  i = 0;
   int j = 0;
   while (c != '\0') {
     c = formatData[j];
-    if isDigit(c) {
+    if (isDigit(c) == 1) {
       buff *= 10;
       buff += c - '0';    
     }
     if (c == ','){
-      formatData[i] = buff
+      formatData[i] = buff;
       i++;
       buff = 0;      
     }
@@ -210,4 +138,98 @@ void broadcast_data(){
 
   //send to lora function
   send_to_lora(packet_addr);
+}
+
+
+void displayBinaryOnLEDs(int decimalNumber) {
+  // Extract individual bits and display on LEDs
+  digitalWrite(ledPin1, (decimalNumber & 0b001) ? HIGH : LOW);
+  digitalWrite(ledPin2, (decimalNumber & 0b010) ? HIGH : LOW);
+  digitalWrite(ledPin3, (decimalNumber & 0b100) ? HIGH : LOW);
+}
+
+//Setup for state machine
+void setup() {
+  // Set LED pins as output
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  //Attempt Lora setup
+  Serial.begin(115200);
+  while (!Serial);
+  if (!LoRa.begin(915E6)) {
+    while (1);
+  }
+}
+
+//method to write to LoRa
+void send_to_lora(uint8_t * packet) {
+  //writing with packet
+  LoRa.beginPacket();
+  LoRa.write(packet, intended_packet_size);
+  LoRa.endPacket();
+}
+
+//TODO::ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent issues with sideways travel
+
+//ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
+float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles
+float MAIN_TRIGGER = 0.0142045; //TEMP main trigger in 8-miles
+
+void loop() {
+  // PLACEHOLDER READ DATA [TODO::Actually get the data]
+  //variable assignment: {acceleration index, altitude, altitude_trend}
+  int data[] = {0,0,0,0,0,0,0};
+  pull_test_data(data);
+
+  //TODO::update method --> data = read_telemetry();
+
+  // Update state machine 
+  switch (StateSet1) {
+    displayBinaryOnLEDs(1);
+    case IDLE_REC: //State to represent the rocket pre launch 
+      //Placeholder condition for detecting launch
+      //index[0] = acceleration index
+      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
+        StateSet1 = DETECT_LAUNCH_IGN;
+      }
+      break;
+
+    //State to represent the rocket post launch (can mistake heavy jostling for launch)
+    case DETECT_LAUNCH_IGN: 
+      displayBinaryOnLEDs(2);
+      //Placeholder condition for confirming launch
+      //index[1] = altitude 
+      //Check if height floor reached and set to valid launch
+      if (data[1] >= HEIGHT_FLOOR) {
+        StateSet1 = VALID_FLIGHT_REC;
+      }
+      //detect false launch 
+      //index[2] = altitude_trend
+      if (data[2] == 0) {
+        //if altitude is not changing
+        StateSet1 = IDLE_REC;
+      }
+      break;
+    
+    //State to represent further confirmation of launch with height floor
+    case VALID_FLIGHT_REC: 
+      displayBinaryOnLEDs(3);
+      //index[2] = atitude trend 
+      //check if altitude is trending downwards
+      if (data[2] < 0) {
+        StateSet1 = DEPLOY_MAIN_REC;
+      }
+      break;
+
+    //State to trigger main deploy at floor
+    case DEPLOY_MAIN_REC: 
+      displayBinaryOnLEDs(4);
+      //deploy_main();
+      //END STATE
+      break;
+    
+  }
+  //send_test_data(StateSet1);
+  //broadcast_data(); //REMOVED FOR PYTHON TESTING
 }
--- Test Code/2_stage_FSM_test.py
+++ Test Code/2_stage_FSM_test.py
@@ -22,7 +22,7 @@ class IMU:
 
 #corresponding class to the Packet struct in the statemachines
 class Packet:
-	def __init__(self, header, stage, latitude, longitude, altitude, imuPacket):
+	def __init__(self, header, stage, latitude, longitude, altitude, alt_trend, imuPacket):
 		self.header = header
 		self.padding0 = 0
 		self.stage = stage
@@ -33,16 +33,18 @@ class Packet:
 		self.padding3 = 0
 		self.altitude = altitude
 		self.padding4 = 0
+		self.alt_tend = alt_trend
+		self.padding5 = 0
 		self.imuPacket = imuPacket
 
 #creates a struct of our data in the proper format for the arduino from our packet object
 def pack_data(packet):
 	# Define the format string for struct.pack
-	format_string = "BxBxlxlxlxllllll"
+	format_string = "BxBxlxlxlxlxllllll"
 	
 	# Pack the data using the format string
 	packed_data = struct.pack(format_string,
-							  packet.header, packet.stage, packet.latitude, packet.longitude, packet.altitude,
+							  packet.header, packet.stage, packet.latitude, packet.longitude, packet.altitude, packet.alt_trend,
 							  packet.imuPacket.xGyro, packet.imuPacket.yGyro, packet.imuPacket.zGyro,
 							  packet.imuPacket.xAcc, packet.imuPacket.yAcc, packet.imuPacket.zAcc)
 	
@@ -55,7 +57,7 @@ def prep_next(df,time_step):
 	full_data = df.iloc[time_step]
 	time_step += 1
 	imu_data = IMU(0, 0, 0, 0, 0, int(full_data["acceleration"]))
-	packet_data_class = Packet(0x55, full_data["state"], 0, 0, int(full_data["height"]), imu_data)
+	packet_data_class = Packet(0x55, full_data["state"], 0, 0, int(full_data["height"]),0, imu_data)
 	struct_data = pack_data(packet_data_class)
 	print("Packed Data:", packed_data)
 	return struct_data, time_step
