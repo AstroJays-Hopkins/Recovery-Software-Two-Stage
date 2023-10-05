98770aa94844b7c4a09f274375bb8f3bd7d951c3
Janya Budaraju <budaraju.janya@gmail.com>
2023-10-05 19:15:52 -0400

    renaming things

---
+++ boosterFSMcode/boosterFSM.ino/Booster FSM.ino
@@ -0,0 +1,235 @@
+#include <LoRa.h>
+
+// Define the possible states for each set
+enum Recovery_State { IDLE_REC = 'I', DETECT_LAUNCH_IGN = 'D', VALID_FLIGHT_REC = 'F', DEPLOY_MAIN_REC = 'M'};
+// Define the LED pins
+const int ledPin1 = 2;
+const int ledPin2 = 3;
+const int ledPin3 = 4;
+
+
+//struct for packet
+typedef struct {
+  uint16_t thetaGyro; //4
+  uint16_t phiGyro; //4
+  uint16_t psiGyro; //4
+  uint16_t xAcc; //4
+  uint16_t yAcc; //4
+  uint16_t zAcc; //4
+
+} __attribute__((packed)) IMU;
+
+typedef struct {
+  char header = 0x55; //1
+  char padding0; //1
+  char stage; //1
+  char padding1; //1
+  uint32_t latitude; //4
+  char padding2; //1
+  uint32_t longitude; //4
+  char padding3; //1
+  uint32_t altitude; //4
+  char padding4; //1
+  uint32_t altTrend;
+  char padding5;  
+  IMU imuPacket; // 24
+} __attribute__((packed)) Packet;
+
+//initialize packet size based on struct
+const int intended_packet_size = sizeof(Packet);
+
+// Define the initial state for each set
+Recovery_State StateSet1 = IDLE_REC;
+
+
+void pull_test_data(int *formatInputString){
+  char inputString[25]; // declare a variable to store the input string
+  int i = 0;
+  while (Serial.available()) { // check if there is any data available in the serial port
+    char c = Serial.read(); // read a character from the serial port
+    if (c == '\n') { // check if the character is a newline character
+      break; // if yes, exit the loop
+    }
+    inputString[i] = c; // append the character to the input string
+    i++;
+  }
+  inputString[i] = '\0';
+  int formatData[9];
+  char c;
+  int buff = 0;
+  i = 0;
+  int j = 0;
+  while (c != '\0') {
+    c = formatData[j];
+    if (isDigit(c) == 1) {
+      buff *= 10;
+      buff += c - '0';    
+    }
+    if (c == ','){
+      formatData[i] = buff;
+      i++;
+      buff = 0;      
+    }
+    j++;
+  }
+  return formatInputString;
+}
+
+void send_test_data(enum Recovery_State state){
+  char outputString[25];
+  Serial.println();
+}
+
+Packet read_telemetry(){
+  char raw_data[50];
+  int data[3];
+  Serial.readBytesUntil("\n", raw_data, 100);
+  int data_index = 0;
+  uint32_t buffered_value = 0;
+  for (int i = 0; i < strlen(raw_data); i++) {//loop through raw chars of serial data input
+    if (raw_data[i] == ','){ //when we see a comma reset the number we are counting 
+      data[data_index] = buffered_value; //store the summed up value
+      data_index += 1; // move on to collecting the next value
+      buffered_value = 0; //reset the buffered value we are counting 
+    }
+    buffered_value = buffered_value*10; //shift left by multiple of 10
+    buffered_value += ((int)raw_data[i] - '0'); //store the next char in the string as an int
+  }
+  IMU imuP;
+  imuP.thetaGyro = 0;
+  imuP.phiGyro = 0;
+  imuP.psiGyro = 0;
+  imuP.xAcc = 0;
+  imuP.yAcc = 0;
+  imuP.zAcc = data[0];
+
+  //set up transmission Packet with fake data
+  Packet packet;
+  packet.stage = '1';
+  packet.latitude = 0;
+  packet.longitude = 10;
+  packet.altitude = data[1];
+  packet.altTrend = data[2];
+  packet.imuPacket = imuP;
+  return packet;
+}
+
+void broadcast_data(){
+  //set up IMU packet with fake data
+  IMU imuP;
+  imuP.thetaGyro = 0;
+  imuP.phiGyro = 0;
+  imuP.psiGyro = 0;
+  imuP.xAcc = 0;
+  imuP.yAcc = 0;
+  imuP.zAcc = 0;
+
+  //set up transmission Packet with fake data
+  Packet packet;
+  packet.stage = '1';
+  packet.latitude = 0;
+  packet.longitude = 10;
+  packet.altitude = 0;
+  packet.altTrend = 0;
+  packet.imuPacket = imuP;
+
+  //convert to uint8_t packet
+  uint8_t * packet_addr = (uint8_t *)(&packet);
+
+  //send to lora function
+  send_to_lora(packet_addr);
+}
+
+
+void displayBinaryOnLEDs(int decimalNumber) {
+  // Extract individual bits and display on LEDs
+  digitalWrite(ledPin1, (decimalNumber & 0b001) ? HIGH : LOW);
+  digitalWrite(ledPin2, (decimalNumber & 0b010) ? HIGH : LOW);
+  digitalWrite(ledPin3, (decimalNumber & 0b100) ? HIGH : LOW);
+}
+
+//Setup for state machine
+void setup() {
+  // Set LED pins as output
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  //Attempt Lora setup
+  Serial.begin(115200);
+  while (!Serial);
+  if (!LoRa.begin(915E6)) {
+    while (1);
+  }
+}
+
+//method to write to LoRa
+void send_to_lora(uint8_t * packet) {
+  //writing with packet
+  LoRa.beginPacket();
+  LoRa.write(packet, intended_packet_size);
+  LoRa.endPacket();
+}
+
+//TODO::ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent issues with sideways travel
+
+//ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
+float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles
+float MAIN_TRIGGER = 0.0142045; //TEMP main trigger in 8-miles
+
+void loop() {
+  // PLACEHOLDER READ DATA [TODO::Actually get the data]
+  //variable assignment: {acceleration index, altitude, altitude_trend}
+  int data[] = {0,0,0,0,0,0,0};
+  pull_test_data(data);
+
+  //TODO::update method --> data = read_telemetry();
+
+  // Update state machine 
+  switch (StateSet1) {
+    displayBinaryOnLEDs(1);
+    case IDLE_REC: //State to represent the rocket pre launch 
+      //Placeholder condition for detecting launch
+      //index[0] = acceleration index
+      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
+        StateSet1 = DETECT_LAUNCH_IGN;
+      }
+      break;
+
+    //State to represent the rocket post launch (can mistake heavy jostling for launch)
+    case DETECT_LAUNCH_IGN: 
+      displayBinaryOnLEDs(2);
+      //Placeholder condition for confirming launch
+      //index[1] = altitude 
+      //Check if height floor reached and set to valid launch
+      if (data[1] >= HEIGHT_FLOOR) {
+        StateSet1 = VALID_FLIGHT_REC;
+      }
+      //detect false launch 
+      //index[2] = altitude_trend
+      if (data[2] == 0) {
+        //if altitude is not changing
+        StateSet1 = IDLE_REC;
+      }
+      break;
+    
+    //State to represent further confirmation of launch with height floor
+    case VALID_FLIGHT_REC: 
+      displayBinaryOnLEDs(3);
+      //index[2] = atitude trend 
+      //check if altitude is trending downwards
+      if (data[2] < 0) {
+        StateSet1 = DEPLOY_MAIN_REC;
+      }
+      break;
+
+    //State to trigger main deploy at floor
+    case DEPLOY_MAIN_REC: 
+      displayBinaryOnLEDs(4);
+      //deploy_main();
+      //END STATE
+      break;
+    
+  }
+  //send_test_data(StateSet1);
+  //broadcast_data(); //REMOVED FOR PYTHON TESTING
+}
---
+++ sustainerFSMcode/sustainerFSM.ino
@@ -0,0 +1,195 @@
+#include <LoRa.h>
+
+// Define the possible states for each set
+enum Recovery_State { IDLE_REC, DETECT_LAUNCH_REC, VALID_FLIGHT_REC, DEPLOY_DROGUE_REC, DEPLOY_MAIN_REC };
+enum Ignition_State { IDLE_IGN, DETECT_LAUNCH_IGN, VALID_FLIGHT_IGN, LOCKOUT_IGN, IGNITE_IGN };
+
+// Define the initial state for each set
+Recovery_State StateSet1 = IDLE_REC;
+Ignition_State StateSet2 = IDLE_IGN;
+
+/*
+stage label bit (1 bit)
+status (1 byte, chars)
+GPS data —> 2 32 bit long  (lat & longitude)
+altimeter —> int 32 bit
+IMU (gyro & accelerometer) —> 6 16 bit integers 
+*/
+
+//struct for packet
+typedef struct {
+  uint16_t xGyro; //4
+  uint16_t yGyro; //4
+  uint16_t zGyro; //4
+  uint16_t xAcc; //4
+  uint16_t yAcc; //4
+  uint16_t zAcc; //4
+
+} __attribute__((packed)) IMU;
+
+typedef struct {
+  char header = 0x55; //1
+  char padding0; //1
+  char stage; //1
+  char padding1; //1
+  uint32_t latitude; //4
+  char padding2; //1
+  uint32_t longitude; //4
+  char padding3; //1
+  uint32_t altitude; //4
+  char padding4; //1
+  IMU imuPacket; // 24
+} __attribute__((packed)) Packet;
+
+//struct for transmission
+const int intended_packet_size = sizeof(Packet);
+
+void setup() {
+  //Lora Setup
+  Serial.begin(115200);
+  while (!Serial);
+  if (!LoRa.begin(915E6)) {
+    while (1);
+  }
+}
+
+void send_to_lora(uint8_t * packet) {
+  //writing with packet
+  LoRa.beginPacket();
+  LoRa.write(packet, intended_packet_size);
+  LoRa.endPacket();
+}
+
+//ENSURE THAT ACCELERATION IS change in height over time NOT absolute accel to prevent isses with sideways travel
+
+//ENSURE THIS IS ALTITUDE ABOVE GROUND NOT FROM SEA
+float HEIGHT_FLOOR = 0.00776714; //TEMP height floor in 8-miles height floor
+float MAIN_TRIGGER = 0.0142045; //TEMP height floor in 8-miles main trigger
+
+void loop() {
+  // PLACEHOLDER READ DATA
+  //variable assignment: {acceleration index, altitude, altitude_trend}
+  int data[] = {0,0,0,0,0,0,0};
+  //data = read_telemetry();
+
+  // Update state machine Recovery State
+  switch (StateSet1) {
+
+    //State to represent the rocket pre launch 
+    case IDLE_REC: 
+      //Placeholder condition for detecting launch
+      //0 = acceleration index
+      //check for change in acceleration and set state to detecting launch
+      if (data[0] >= 0) {
+        StateSet1 = DETECT_LAUNCH_REC;
+      }
+      break;
+    
+    //state to represent the rocket post launch (can mistake heavy jostling for launch)
+    case DETECT_LAUNCH_IGN:
+      //Placeholder condition for confirming launch
+      //1 = altitude 
+      if (data[1] >= HEIGHT_FLOOR) { 
+        //height floor reached
+        StateSet1 = VALID_FLIGHT_REC;
+      }
+      //detect false launch by looking for altitude trend
+      //2 = altitude_trend
+      if (data[2] == 0) {
+        StateSet1 = IDLE_REC;
+      }
+      break;
+
+    //State to represent further confirmation of launch with height floor
+    case VALID_FLIGHT_REC:
+      //Placeholder condition for detecting apogee and triggering drouge deploy
+      if (data[2] < 0) {//if treding down
+        StateSet1 = DEPLOY_DROGUE_REC;
+      }
+      break;
+    
+    //State to trigger drogue deploy at apogee
+    case DEPLOY_DROGUE_REC: 
+      //deploy_drogue();
+      if (data[1] >= MAIN_TRIGGER) {
+        StateSet1 = DEPLOY_MAIN_REC; 
+      }
+      break;
+    
+    //State to trigger main deploy at floor
+    case DEPLOY_MAIN_REC: 
+      //deploy_main();
+      //END STATE
+      break;
+  }
+
+  // Update state machine for Ignition State
+  switch (StateSet2) {
+    //rocket idle state
+    case IDLE_IGN:
+      //Placeholder condition for detecting launch
+      if (data[0] >= 0){ //movement of rocket is the current assumption for launch
+        StateSet2 = DETECT_LAUNCH_IGN;
+      }
+      break;
+    //pre-official launch to account for false launches
+    case DETECT_LAUNCH_IGN:
+      //Placeholder condition for confirming launch
+      if (data[1] >= HEIGHT_FLOOR) { //floor reached
+        StateSet2 = VALID_FLIGHT_IGN;
+      }
+      //detect false launch  (we dropped the rocket)
+      if (data[2] == 0) {//if not tredning anywhere
+        StateSet2 = IDLE_IGN;
+      }
+      break;
+    //once valid flight established
+    case VALID_FLIGHT_IGN:
+      //if any tilt exists initiate lockout to avoid Missiling
+      if (abs(data[3]) >= 5) {//
+        StateSet2 = LOCKOUT_IGN;
+      }
+      // detect time to ignite based on slowing down
+      if (data[4] < 0) {
+        StateSet2 = IGNITE_IGN;
+      }
+      break;
+      StateSet2 = LOCKOUT_IGN;
+      break;
+    case LOCKOUT_IGN:
+      //END STATE
+      break;
+    case IGNITE_IGN:
+      //do_ignite();
+      //END STATE
+      break;
+  }
+  broadcast_data();
+  // Add any other code you want to run continuously in your loop here
+}
+
+
+void broadcast_data() {
+  //set up IMU packet
+  IMU imuP;
+  imuP.xGyro = 0;
+  imuP.yGyro = 0;
+  imuP.zGyro = 0;
+  imuP.xAcc = 0;
+  imuP.yAcc = 0;
+  imuP.zAcc = 0;
+
+  //set up transmission Packet
+  Packet packet;
+  packet.stage = '1';
+  packet.latitude = 0;
+  packet.longitude = 10;
+  packet.altitude = 0;
+  packet.imuPacket = imuP;
+
+  //convert to uint8_t packet
+  uint8_t * packet_addr = (uint8_t *)(&packet);
+
+  //send to lora function
+  send_to_lora(packet_addr);
+}
